8fbd7bbf0fba3fe5d438935eef0fb93c4f29fb11
Joseph <before30@gmail.com>
2019-02-20 00:42:42 +0900

    Add envirnoment file

---
+++ sample-springboot2/out/production/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO sample_customer(u_id, u_first, u_last, created_at, last_modified_at)
+        values (1, '1_test_first', '1_test_last', NOW(), NOW());
+
+INSERT INTO sample_customer(u_id, u_first, u_last, created_at, last_modified_at)
+        values (2, '2_test_first', '2_test_last', NOW(), NOW());
+INSERT INTO sample_customer(u_id, u_first, u_last, created_at, last_modified_at)
+        values (3, '3_test_first', '3_test_last', NOW(), NOW());
+INSERT INTO sample_customer(u_id, u_first, u_last, created_at, last_modified_at)
+        values (4, '4_test_first', '4_test_last', NOW(), NOW());
+INSERT INTO sample_customer(u_id, u_first, u_last, created_at, last_modified_at)
+        values (5, '5_test_first', '5_test_last', NOW(), NOW());
+INSERT INTO sample_customer(u_id, u_first, u_last, created_at, last_modified_at)
+        values (6, '6_test_first', '6_test_last', NOW(), NOW());
+INSERT INTO sample_customer(u_id, u_first, u_last, created_at, last_modified_at)
+        values (7, '7_test_first', '7_test_last', NOW(), NOW());
